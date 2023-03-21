21d083e597f96e46c752af1c523f8099597b0ce3
你手捧鲜花 <157215725@qq.com>
2023-03-21 14:58:52 +0800

    init

--- README.md
+++ README.md
@@ -1,35 +1,31 @@
-<h1 align="center">starter-ts-cli</h1>
+<h1 align="center">ootp</h1>
 
 <p align="center">
-  <a href="https://unpkg.com/starter-ts-cli">
-    <img src="https://img.shields.io/bundlephobia/minzip/starter-ts-cli" alt="Minzip">
+  <a href="https://unpkg.com/ootp">
+    <img src="https://img.shields.io/bundlephobia/minzip/ootp" alt="Minzip">
   </a>
-  <a href="https://www.npmjs.com/package/starter-ts-cli">
-    <img src="https://img.shields.io/npm/v/starter-ts-cli.svg" alt="Version">
+  <a href="https://www.npmjs.com/package/ootp">
+    <img src="https://img.shields.io/npm/v/ootp.svg" alt="Version">
   </a>
-  <a href="https://www.npmjs.com/package/starter-ts-cli">
-    <img src="https://img.shields.io/npm/dm/starter-ts-cli" alt="Downloads">
+  <a href="https://www.npmjs.com/package/ootp">
+    <img src="https://img.shields.io/npm/dm/ootp" alt="Downloads">
   </a>
-  <a href="https://github.com/qq15725/starter-ts-cli/issues">
-    <img src="https://img.shields.io/github/issues/qq15725/starter-ts-cli" alt="Issues">
+  <a href="https://github.com/qq15725/ootp/issues">
+    <img src="https://img.shields.io/github/issues/qq15725/ootp" alt="Issues">
   </a>
-  <a href="https://github.com/qq15725/starter-ts-cli/blob/main/LICENSE">
-    <img src="https://img.shields.io/npm/l/starter-ts-cli.svg" alt="License">
+  <a href="https://github.com/qq15725/ootp/blob/main/LICENSE">
+    <img src="https://img.shields.io/npm/l/ootp.svg" alt="License">
   </a>
 </p>
 
-## Try it now!
+## 📦 Install
 
-### GitHub Template
+```shell
+npm i -g ootp
+```
 
-[Create a repo from this template on GitHub](https://github.com/qq15725/starter-ts-cli/generate).
+## 🦄 Usage
 
-### Clone to local
-
-If you prefer to do it manually with the cleaner git history
-
-```bash
-npx degit qq15725/starter-ts-cli my-ts-cli
-cd my-ts-cli
-pnpm i # If you don't have pnpm installed, run: npm install -g pnpm
+```shell
+ootp "you-key"
 ```
--- src/create-cli.ts
+++ src/create-cli.ts
@@ -1,9 +1,79 @@
+import crypto from 'node:crypto'
 import { cac } from 'cac'
+import consola from 'consola'
 import { bin, version } from '../package.json'
 import type { Options } from './types'
 
+const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
+
+function base32ToBuf(str: string) {
+  let end = str.length
+  while (str[end - 1] === '=') --end
+  const cstr = (end < str.length ? str.substring(0, end) : str).toUpperCase()
+
+  const buf = new ArrayBuffer(((cstr.length * 5) / 8) | 0)
+  const arr = new Uint8Array(buf)
+  let bits = 0
+  let value = 0
+  let index = 0
+
+  for (let i = 0; i < cstr.length; i++) {
+    const idx = ALPHABET.indexOf(cstr[i])
+    if (idx === -1) throw new TypeError(`Invalid character found: ${ cstr[i] }`)
+    value = (value << 5) | idx
+    bits += 5
+    if (bits >= 8) {
+      bits -= 8
+      arr[index++] = value >>> bits
+    }
+  }
+
+  return arr
+}
+
+function uintToBuf(num: number) {
+  const buf = new ArrayBuffer(8)
+  const arr = new Uint8Array(buf)
+  let acc = num
+  for (let i = 7; i >= 0; i--) {
+    if (acc === 0) break
+    arr[i] = acc & 255
+    acc -= arr[i]
+    acc /= 256
+  }
+  return arr
+}
+
 export function createCli(_options: Options) {
   const cli = cac(Object.keys(bin)[0])
+
+  cli
+    .command('<secret>', 'Generate TOTP code')
+    .action((secret, commandOptions) => {
+      const {
+        algorithm = 'sha1',
+        digits = 6,
+        period = 30,
+        timestamp = Date.now(),
+      } = commandOptions
+      const counter = Math.floor(timestamp / 1000 / period)
+
+      const key = base32ToBuf(secret)
+      const message = uintToBuf(counter)
+      const hmac = crypto.createHmac(algorithm, key)
+      hmac.update(message)
+      const digest = new Uint8Array(hmac.digest().buffer)
+
+      const offset = digest[digest.byteLength - 1] & 15
+      const otp = (
+        ((digest[offset] & 127) << 24)
+        | ((digest[offset + 1] & 255) << 16)
+        | ((digest[offset + 3] & 255) << 8)
+        | (digest[offset + 3] & 255)
+      ) % 10 ** digits
+
+      consola.log(String(otp).padStart(digits, '0'))
+    })
 
   cli
     .help()
